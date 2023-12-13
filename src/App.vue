4c090b751066bb809438159b19af78bb375c62ae
Camille-Durand <camillefanguin@gmail.com>
2023-12-13 16:52:24 +0100

    newAmi

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
     <!-- <div class="container"> -->
       <h1 class="text-center">Les Amis</h1>
       <ul class="list-group">
+        <new-ami v-on:add-ami="ajouterAmi">
+        </new-ami>
+
         <un-ami v-for="unAmi in lesAmis" 
         v-bind:key="unAmi.id" :id="unAmi.id" :leNom="unAmi.name" :lePhone="unAmi.phone" :leMail="unAmi.email" :premium="unAmi.premium" 
-        @mon-event-premium="afficherStatusPremium(unAmi.id)">
+        @mon-event-premium="afficherStatusPremium">
         </un-ami>
       </ul>
     <!-- </div> -->
@@ -36,6 +39,16 @@ export default{
             let unAmiIdentified = this.lesAmis.find(unTruc => unTruc.id === leIdDansUnAmi);
             unAmiIdentified.premium = !unAmiIdentified.premium;
             console.log(unAmiIdentified);
+        },
+        ajouterAmi(name, phone, email) {
+            let newAmiContact = {
+                id: new Date().toISOString(),
+                name: name,
+                phone: phone,
+                email: email,
+                premium: false
+            };
+            this.lesAmis.push(newAmiContact);
         }
     }
 }
---
+++ src/components/NewAmi.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container bg-info my-1">
+        <form @submit.prevent="submitData()">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Name</label>
+            <input  class="form-control form-control-sm" type="text" placeholder="votre nom" aria-label=".form-control-sm example"
+            v-model="enteredName">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Tél</label>
+            <input class="form-control form-control-sm" type="tel" placeholder="Votre Tél" aria-label=".form-control-sm example"
+            v-model="enteredPhone">
+        </div>       
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email</label>
+            <input  class="form-control form-control-sm" type="email" placeholder="Votre EMAIL" aria-label=".form-control-sm example"
+            v-model="enteredMail">
+        </div>     
+        <button class="btn btn-primary btn-sm col-12 my-1">Ajouter Ami</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        emits: ['add-ami'],
+        data() {
+            return {
+            enteredName: '',
+            enteredPhone: '',
+            enteredMail: ''
+            }
+        },
+        methods: {
+            submitData() {
+                this.$emit('add-ami',this.enteredName,this.enteredPhone,this.enteredMail);
+            }
+        }
+    }
+</script>
